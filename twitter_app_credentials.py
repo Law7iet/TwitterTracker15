c335cac2a70fae64de3aa4f0076942f55433ddbc
Pierpaolo Faustini <iierpaolo.faustini@gmail.com>
2020-11-14 11:11:08 +0100

    Deleted Pit's files on master branch and added them to Pit's branch

--- twitter_app_credentials.py
+++
@@ -1,7 +0,0 @@
-# Twitter app credentials
-
-consumer_key = 'YOUR_CONSUMER_KEY'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
